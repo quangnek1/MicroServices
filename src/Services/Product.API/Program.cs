dd553c116264a137f337d5ac74fc2bfd6f1af3e1
quangnek1 <leduyquang1201@gmail.com>
2025-06-06 17:49:07 +0700

    tao serilog

--- src/Services/Product.API/Program.cs
+++ src/Services/Product.API/Program.cs
@@ -1,25 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
+Log.Information("Starting Product API up");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	var builder = WebApplication.CreateBuilder(args);
+
+	builder.Host.UseSerilog();
+
+	// Add services to the container.
+
+	builder.Services.AddControllers();
+	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
+
+}
+catch (Exception ex)
+{
+
+	Log.Fatal(ex, messageTemplate: "Unhandled exception");
+}
+finally
+{
+	Log.Information(messageTemplate: "Shut down Product API complete");
+	Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
